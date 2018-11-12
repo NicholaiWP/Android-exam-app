1b0ad82630b354184cd85352c8be0ecd0547fe6e
Unknown <nicholaipedersen745@gmail.com>
2018-11-12 12:12:54 +0100

    First push

---
+++ app/src/main/java/com/example/nicholai/examdiaryapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.nicholai.examdiaryapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String beginning;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        beginning = "welcome";
+    }
+}
